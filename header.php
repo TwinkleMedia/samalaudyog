4542dd0c87e725a66d3a801212b16589949f1456
TwinkleMedia <hs0052264@gmail.com>
2025-02-23 21:43:34 +0530

    a

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
             <img src="./assest/logo/Asset 3300.png" alt="Samala-Logo" >
         </div>
        <ul class="links">
-           <li><a href="./index.php">Home</a></li>
-           <li><a href="./aboutus.php">About</a></li>
-           <li><a href="./products.php">Products</a></li>
-           <li><a href="./career.php">Career</a></li>
-           <li><a href="./contact.php">Contact</a></li>
+           <li><a href="/index.php">Home</a></li>
+           <li><a href="/aboutus.php">About</a></li>
+           <li><a href="/products.php">Products</a></li>
+           <li><a href="/career.php">Career</a></li>
+           <li><a href="/contact.php">Contact</a></li>
        </ul>
        <label for="nav-toggle" class="icon-burger">
            <div class="line"></div>
