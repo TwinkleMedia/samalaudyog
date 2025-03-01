5539a4c95eebe109b09799ffa9a6eab4eeb3783a
TwinkleMedia <hs0052264@gmail.com>
2025-03-01 16:06:56 +0530

    text1

--- index.php
+++ index.php
@@ -73,6 +73,43 @@ $conn->close();
   </div>
 </div>
 
+ <!-- -----------------About -section     -->
+   
+ <section class="about-section mt-3">
+        <div class="container">
+            <div class="row">
+                <div class="content-column col-lg-6 col-md-12 col-sm-12 order-2" data-aos="fade-left">
+                    <div class="inner-column">
+                        <div class="sec-title">
+                            <span class="title">About </span>
+                            <h2>Nature-Driven Innovation for a Better, Safer World</h2>
+                        </div>
+                        <div class="text">Samala Udyog is an innovation-driven start-up that is dedicated to providing innovative products made with natural chemicals and special clay minerals to customers who prioritize the environment and health. The company was founded by a duo with a combined 40 years of experience in the chemical industry and a passion for creating sustainable and effective products.</div>
+                        <div class="text">
+                            With a focus on natures’ bounty, we are committed to providing our customers with the best of natural ingredients for Home care, Personal care, and Industrial care applications. Join us in our mission to create a healthier and more sustainable world, one natural product at a time.
+                        </div>
+                        <div class="btn-box">
+                            <a href="./contact.php" class="theme-btn btn-style-one">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Image Column -->
+                <div class="image-column col-lg-6 col-md-12 col-sm-12" data-aos="fade-right">
+                    <div class="inner-column wow fadeInLeft">
+                        <div class="author-desc">
+                            <h2></h2>
+                            <span>Founder</span>
+                        </div>
+                        <figure class="image-1"><a href="#" class="lightbox-image" data-fancybox="images"><img title="Rahul Kumar Yadav" src="https:./assest/Founder.png" alt=""></a></figure>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
 
 
 
