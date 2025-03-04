878c0cd036cb56ee5309c2fb5f377c95d46cf733
TwinkleMedia <hs0052264@gmail.com>
2025-03-04 22:35:11 +0530

    video

--- index.php
+++ index.php
@@ -253,7 +253,7 @@ foreach ($productsByCategory as $category => $products) {
         <div class="swiper-slide">
           <div class="video-container">
             <video class="video-embed" controls playsinline muted>
-              <source src="./assest/SAMALA_01.mp4" type="video/mp4">
+              <source src="./assest/video1.mp4" type="video/mp4">
               Your browser does not support the video tag.
             </video>
           </div>
