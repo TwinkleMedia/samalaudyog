903513f18232a9748d98422e1a2d0a72d086e59d
TwinkleMedia <hs0052264@gmail.com>
2025-03-15 11:31:36 +0530

    h

--- header.php
+++ header.php
@@ -1,19 +1,25 @@
-<nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="./assest/logo/Asset 3300.png" alt="Samala-Logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="./index.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./aboutus.php">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./products.php">Products</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./career.php">Career</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./contact.php">Contact</a></li>
-                </ul>
-            </div>
+<nav>
+        <input type="checkbox" id="nav-toggle">
+        <div class="logo">
+            <img src="./assest/logo/Asset 3300.png" alt="Samala-Logo" >
         </div>
+       <ul class="links">
+           <li><a href="./index.php">Home</a></li>
+           <li><a href="./aboutus.php">About</a></li>
+           <li><a href="./products.php">Products</a></li>
+           <li><a href="./career.php">Career</a></li>
+           <li><a href="./contact.php">Contact</a></li>
+       </ul>
+       <label for="nav-toggle" class="icon-burger">
+           <div class="line"></div>
+           <div class="line"></div>
+           <div class="line"></div>
+       </label>
     </nav>
+
+   
+    <label for="nav-toggle" class="icon-burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </label>
