cf99f884160e180075e121abc3f037ba1f502d05
TwinkleMedia <hs0052264@gmail.com>
2025-04-03 15:12:53 +0530

    an

--- mainlink.php
+++ mainlink.php
@@ -42,3 +42,14 @@
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-T0CSDM62TF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T0CSDM62TF');
+</script>
