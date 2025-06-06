145201c42996e7aeb27c07f6adaf6abbe6927501
TwinkleMedia <hs0052264@gmail.com>
2025-06-06 17:20:54 +0530

    update

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
      <?php include './mainlink.php'  ?>
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPMJZ86J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
 <?php include './header.php' ?>
 <?php
 error_reporting(E_ALL);
